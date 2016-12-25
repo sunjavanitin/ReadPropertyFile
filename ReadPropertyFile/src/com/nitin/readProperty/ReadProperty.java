fab0dc1085fa3a614f3b8d5cc69155fce04b2712
oranitin <oranitin@oranitin-ThinkPad-T410>
2016-12-25 16:36:11 +0530

    Read Property File In java

---
+++ ReadPropertyFile/src/com/nitin/readProperty/ReadProperty.java
@@ -0,0 +1,33 @@
+package com.nitin.readProperty;
+
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.util.Enumeration;
+import java.util.Properties;
+
+public class ReadProperty {
+
+	public static void main(String[] args) {
+		try {
+			File file = new File("nitin.properties");
+			FileInputStream fileInput = new FileInputStream(file);
+			Properties properties = new Properties();
+			properties.load(fileInput);
+			fileInput.close();
+
+			Enumeration enuKeys = properties.keys();
+			while (enuKeys.hasMoreElements()) {
+				String key = (String) enuKeys.nextElement();
+				String value = properties.getProperty(key);
+				System.out.println(key + ": " + value);
+			}
+		} catch (FileNotFoundException e) {
+			e.printStackTrace();
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+	}
+
+}
